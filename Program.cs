2ae6c433f5fafc2acfa6676ca14c3f325bc040ad
gabrielmarcos2 <gabrielsqueda123@gmail.com>
2022-02-25 22:05:47 -0300

    finalizar o abreu

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Mongaguá");
+Console.WriteLine("Santos");
+Console.WriteLine("São Vicente");
+Console.WriteLine("Guarujá");
+Console.WriteLine("Peruibe");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Praia Grande");
